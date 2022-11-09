269ceaa185fe3538ab5c41b8e3964b947e579999
Qiping Pan <panqp@gl-login2.arc-ts.umich.edu>
2022-11-09 16:07:04 -0500

    initial commit

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=example1
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=1
+#SBATCH --time=00:01:00
+#SBATCH --account=eecs587f22_class
+#SBATCH --partition=gpu
+
+
+
+
+#make sure to load the cuda module before running
+#module load cuda
+#make sure to compile your program using nvcc
+#nvcc -o example1 example1.cu
+ nvidia-smi -q
