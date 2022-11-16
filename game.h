3a80559fef7f67d9472aface45a5021320b73e4f
Qiping Pan <panqp@gl-login1.arc-ts.umich.edu>
2022-11-16 10:47:43 -0500

    configurable board size

--- game.h
+++ game.h
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <string>
 #include <set>
+const int BOARD_SIZE = 4;
 using namespace std;
 enum class ROLE{
     BLACK,
@@ -40,18 +41,19 @@ public:
     State s[9][9];
     ROLE current_role;
     Board():current_role(ROLE::WHITE){
-        for(auto i = 0; i < 8; i++){
-            for(auto j = 0; j < 8; j++){
+        for(auto i = 0; i < BOARD_SIZE; i++){
+            for(auto j = 0; j < BOARD_SIZE; j++){
                 s[i][j] = State::NONE;
             }
         }
-        s[3][3] = State::BLACK;
-        s[3][4] = State::WHITE;
-        s[4][3] = State::WHITE;
-        s[4][4] = State::BLACK;
+        int center = BOARD_SIZE/2;
+        s[center-1][center-1] = State::BLACK;
+        s[center-1][center] = State::WHITE;
+        s[center][center-1] = State::WHITE;
+        s[center][center] = State::BLACK;
     };
     bool update(Action action){
-        if(action.y >= 8 || action.x >= 8 || action.y < 0 || action.x < 0) throw "Out of board!";
+        if(action.y >= BOARD_SIZE || action.x >= BOARD_SIZE || action.y < 0 || action.x < 0) throw "Out of board!";
         if(s[action.y][action.x] != State::NONE) throw "This position is already filled!";
         bool legal = false;
         vector<Action> v = get_actions();
@@ -79,10 +81,10 @@ public:
         // bottom
         y = action.y+1;
         x = action.x;
-        while(y < 8 && s[y][x] == opponentStone){
+        while(y < BOARD_SIZE && s[y][x] == opponentStone){
             y++;
         }
-        if(y < 8 && s[y][x] == myStone){
+        if(y < BOARD_SIZE && s[y][x] == myStone){
             for(int i = action.y+1; i < y; i++){
                 s[i][x] = myStone;
             }
@@ -91,10 +93,10 @@ public:
         // right
         y = action.y;
         x = action.x+1;
-        while(x < 8 && s[y][x] == opponentStone){
+        while(x < BOARD_SIZE && s[y][x] == opponentStone){
             x++;
         }
-        if(x < 8 && s[y][x] == myStone){
+        if(x < BOARD_SIZE && s[y][x] == myStone){
             for(int i = action.x+1; i < x; i++){
                 s[y][i] = myStone;
             }
@@ -131,12 +133,12 @@ public:
         y = action.y-1;
         x = action.x+1;
         count = 0;
-        while(x < 8 && y >= 0 && s[y][x] == opponentStone){
+        while(x < BOARD_SIZE && y >= 0 && s[y][x] == opponentStone){
             x++;
             y--;
             count ++;
         }
-        if(x < 8 && y >= 0 && s[y][x] == myStone){
+        if(x < BOARD_SIZE && y >= 0 && s[y][x] == myStone){
             for(int i = 0; i < count; i++){
                 s[action.y-1-i][action.x+1+i] = myStone;
             }
@@ -146,12 +148,12 @@ public:
         y = action.y+1;
         x = action.x+1;
         count = 0;
-        while(x < 8 && y < 8 && s[y][x] == opponentStone){
+        while(x < BOARD_SIZE && y < BOARD_SIZE && s[y][x] == opponentStone){
             x++;
             y++;
             count ++;
         }
-        if(x < 8 && y < 8 && s[y][x] == myStone){
+        if(x < BOARD_SIZE && y < BOARD_SIZE && s[y][x] == myStone){
             for(int i = 0; i < count; i++){
                 s[action.y+1+i][action.x+1+i] = myStone;
             }
@@ -162,12 +164,12 @@ public:
         y = action.y+1;
         x = action.x-1;
         count = 0;
-        while(x >=0 && y < 8 && s[y][x] == opponentStone){
+        while(x >=0 && y < BOARD_SIZE && s[y][x] == opponentStone){
             x--;
             y++;
             count ++;
         }
-        if(x >= 0 && y < 8 && s[y][x] == myStone){
+        if(x >= 0 && y < BOARD_SIZE && s[y][x] == myStone){
             for(int i = 0; i < count; i++){
                 s[action.y+1+i][action.x-1-i] = myStone;
             }
@@ -192,8 +194,8 @@ public:
         
         State myStone = (current_role == ROLE::BLACK) ? State::BLACK : State::WHITE;
         State opponentStone = (current_role == ROLE::BLACK) ? State::WHITE : State::BLACK;
-        for(int i = 0; i < 8; i++){
-            for(int j = 0; j < 8; j++){
+        for(int i = 0; i < BOARD_SIZE; i++){
+            for(int j = 0; j < BOARD_SIZE; j++){
                 if(s[i][j] == myStone){
                     // top
                     int y = i-1;
@@ -206,18 +208,18 @@ public:
                     // bottom
                     y = i+1;
                     x = j;
-                    while(y < 8 && s[y][x] == opponentStone){
+                    while(y < BOARD_SIZE && s[y][x] == opponentStone){
                         y++;
                     }
-                    if(y < 8 && y != i+1 && s[y][x] == State::NONE) sa.emplace(y,x);
+                    if(y < BOARD_SIZE && y != i+1 && s[y][x] == State::NONE) sa.emplace(y,x);
 
                     // right
                     y = i;
                     x = j+1;
-                    while(x < 8 && s[y][x] == opponentStone){
+                    while(x < BOARD_SIZE && s[y][x] == opponentStone){
                         x++;
                     }
-                    if(x < 8 && x != j+1 && s[y][x] == State::NONE) sa.emplace(y,x);
+                    if(x < BOARD_SIZE && x != j+1 && s[y][x] == State::NONE) sa.emplace(y,x);
 
                     // left
                     y = i;
@@ -240,20 +242,20 @@ public:
                     // top right
                     y = i-1;
                     x = j+1;
-                    while(x < 8 && y >= 0 && s[y][x] == opponentStone){
+                    while(x < BOARD_SIZE && y >= 0 && s[y][x] == opponentStone){
                         x++;
                         y--;
                     }
-                    if(x < 8 && y >= 0 && x!= j+1 && s[y][x] == State::NONE) sa.emplace(y,x);
+                    if(x < BOARD_SIZE && y >= 0 && x!= j+1 && s[y][x] == State::NONE) sa.emplace(y,x);
 
                     // bottom left
                     y = i+1;
                     x = j-1;
-                    while(x >= 0 && y < 8 && s[y][x] == opponentStone){
+                    while(x >= 0 && y < BOARD_SIZE && s[y][x] == opponentStone){
                         x--;
                         y++;
                     }
-                    if(x >= 0 && y < 8 && x != j-1 && s[y][x] == State::NONE) sa.emplace(y,x);
+                    if(x >= 0 && y < BOARD_SIZE && x != j-1 && s[y][x] == State::NONE) sa.emplace(y,x);
 
 
                     // bottom right
@@ -263,7 +265,7 @@ public:
                         x++;
                         y++;
                     }
-                    if(x < 8 && y < 8 && x != j+1 && s[y][x] == State::NONE) sa.emplace(y,x);
+                    if(x < BOARD_SIZE && y < BOARD_SIZE && x != j+1 && s[y][x] == State::NONE) sa.emplace(y,x);
                 }
             }
         }
@@ -272,8 +274,8 @@ public:
     }; // TODO
     Result get_result(){
         int count = 0;
-        for(int i = 0; i < 8; i++){
-            for(int j = 0; j < 8; j++){
+        for(int i = 0; i < BOARD_SIZE; i++){
+            for(int j = 0; j < BOARD_SIZE; j++){
                 if(s[i][j] == State::BLACK) count ++;
                 if(s[i][j] == State::WHITE) count --;
             }
@@ -295,9 +297,9 @@ public:
     }
     void print(){
         cout << "  0 1 2 3 4 5 6 7" << endl;
-        for(int i = 0; i < 8; i++){
+        for(int i = 0; i < BOARD_SIZE; i++){
             cout << i << ' ';
-            for(int j = 0; j < 8; j++){
+            for(int j = 0; j < BOARD_SIZE; j++){
 
                 if(s[i][j] == State::BLACK) cout << 'x';
                 if(s[i][j] == State::WHITE) cout << 'o';
